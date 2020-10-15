e8520899721e3bc47756ac2ac8fd9ae3af89ec7c
VyzhD <vyzhgin.dmitryy@gmail.com>
2020-10-16 00:13:09 +0300

    implemented sound playing on load after delay

---
+++ scripts/sound.js
@@ -0,0 +1,26 @@
+    function musik_test(){
+        function  date (){
+        var today = new Date();
+        var dd = String(today.getDate()).padStart(2, '0');
+        var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        var yyyy = today.getFullYear();
+
+        today = mm + '_' + dd + '_' + yyyy;
+        return today;
+        };
+
+
+        data = date();
+
+        setTimeout(soundClick, 15000);
+
+
+
+        function  soundClick()
+        {
+            scr = "sound.mp3";
+            var audio2 = new Audio(scr);
+            audio2.play();
+        }
+
+    };
